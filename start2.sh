2aa03f0e66122be23f069f61f4a6e5c15e3f39ea
Jozsef Valkai <jozsef.valkai@comp280.ssc.luxoffice.duodecadits.com>
2018-08-23 09:02:16 +0200

    modified startup script

--- start2.sh
+++ start2.sh
@@ -3,10 +3,10 @@
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
 DMURL="https://github.com/docker/machine/releases/download/v0.15.0";
 DCURL="https://github.com/docker/compose/releases/download/1.22.0"
-REPODIR="my_first_docker_prod";
+REPODIR="docker-test";
 DOCKERMACHINE="docker-sandbox";
-REPO="https://github.com/netuddmeg/my_first_docker_prod.git";
-PROJECTNAME="my_first_docker_project";
+REPO="https://github.com/netuddmeg/docker-test.git";
+PROJECTNAME="docker-test";
 DPATH="/usr/local/bin/";
 
 export DEBIAN_FRONTEND=noninteractive && sudo apt install \
@@ -34,8 +34,8 @@ fi;
 		docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN $DOCKERMACHINE;
 
 		docker-machine ssh $DOCKERMACHINE "export DEBIAN_FRONTEND=noninteractive && \
-										sudo apt-get install git npm -y && \
-										sudo npm install express;"
+										sudo apt-get install git -y;"
+										
 
 #		docker-machine ssh $DOCKERMACHINE "curl -L $DMURL/docker-machine-$(uname -s)-$(uname -m) > $DPATH/docker-machine && chmod +x $DPATH/docker-machine";
 		eval $(docker-machine env $DOCKERMACHINE);
